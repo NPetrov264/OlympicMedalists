32199da7c359c2370a12ebf52c29550a8c9e0540
NPetrov <n.petrov264@gmail.com>
2024-08-13 18:12:44 +0300

    Configured sql databases

--- src/main/java/com/npetrov/OlympicMedalists/OlympicMedalistsApplication.java
+++ src/main/java/com/npetrov/OlympicMedalists/OlympicMedalistsApplication.java
@@ -1,4 +1,4 @@
-package com.example.OlympicMedalists;
+package com.npetrov.OlympicMedalists;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/npetrov/OlympicMedalists/OlympicMedalistsApplicationTests.java
+++ src/test/java/com/npetrov/OlympicMedalists/OlympicMedalistsApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.OlympicMedalists;
+package com.npetrov.OlympicMedalists;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
