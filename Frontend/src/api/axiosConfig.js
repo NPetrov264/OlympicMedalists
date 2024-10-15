2aca9a0c0ab0d71172126337b58c45fd843c5f81
NPetrov <n.petrov264@gmail.com>
2024-10-15 19:03:50 +0300

    Update axiosConfig

--- Frontend/src/api/axiosConfig.js
+++ Frontend/src/api/axiosConfig.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 export default axios.create({
-    baseURL:'http://localhost:8080/',
+    baseURL: SERVER_BASE_URL,
     headers:{"ngrok-skip-browser-warning": "true"}
 });
